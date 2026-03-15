387b721f0a0b8999fadd73d175c3084fd9fe5bac
Manar <hamraouimanar812@gmail.com>
2026-03-15 15:02:22 +0000

    Clean1

--- app/user_manager.py
+++ app/user_manager.py
@@ -1,23 +1,17 @@
 class UserManager:
-
     def __init__(self):
         self.users = []
 
     def add_user(self, username):
         if not username:
             raise ValueError("Le nom d'utilisateur est obligatoire")
-
         if username in self.users:
             raise ValueError("Utilisateur deja existant")
-
-
         self.users.append(username)
 
     def remove_user(self, username):
         if username not in self.users:
             raise ValueError("Utilisateur introuvable")
-
-
         self.users.remove(username)
 
     def count_users(self):
