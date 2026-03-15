7fb8518a0641ec94c3a79121755a2752f2bf0599
Manar <hamraouimanar812@gmail.com>
2026-03-15 13:02:08 +0000

    Ajout du code et du pipeline multi-jobs

--- tests/test_user_manager.py
+++ tests/test_user_manager.py
@@ -4,25 +4,25 @@ from app.user_manager import UserManager
 
 def test_add_user():
     manager = UserManager()
-    manager.add_user("Adam")
+    manager.add_user("Manar")
     assert manager.count_users() == 1
 
 
 def test_add_existing_user():
     manager = UserManager()
-    manager.add_user("Adam")
+    manager.add_user("Manar")
     with pytest.raises(ValueError):
-        manager.add_user("Adam")
+        manager.add_user("Manar")
 
 
 def test_remove_user():
     manager = UserManager()
-    manager.add_user("Youssef")
-    manager.remove_user("Youssef")
+    manager.add_user("Hiba")
+    manager.remove_user("Hiba")
     assert manager.count_users() == 0
 
 
 def test_remove_unknown_users():
     manager = UserManager()
     with pytest.raises(ValueError):
-        manager.remove_user("Saad")
+        manager.remove_user("Test")
